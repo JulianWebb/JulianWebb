76badbc07290639d9e4092786e00a8befa321b82
Julian Webb <git@julianwebb.ca>
2021-06-25 01:09:05 -0700

    Added Favicon, Updated Text on Index

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1b0c4512713811b756a7"; }
+/******/ 		__webpack_require__.h = function() { return "24101f80f13e23427376"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
